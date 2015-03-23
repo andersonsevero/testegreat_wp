0d73ca89e0513835794830dcd5591959b38665f0
Severo <andersonsevero@gmail.com>
2015-03-23 15:52:50 -0700

    Initial commit: pushing original files from GREat

---
+++ WindowsExercise01/WindowsExercise01/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AsyncAwaitDojo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
